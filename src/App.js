481237f244b1c9c68611a0c83ac999dad0f538a7
Anush Kumar <17anushkumar@gmail.com>
2024-04-16 15:34:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,115 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { getDatabase, push, ref, set, onChildAdded } from "firebase/database";
+import ak from './ak.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [name, setname] = useState('');
+    const [chats, setchats] = useState([]);
+    const [msg, setmsg] = useState('');
+
+    const db = getDatabase();
+    const chatListRef = ref(db, 'chats');
+
+    const updateHeight = () => {
+        const element = document.getElementById('chat');
+        if (element) {
+            element.scrollTop = element.scrollHeight;
+        }
+    }
+
+    useEffect(() => {
+        onChildAdded(chatListRef, (data) => {
+            setchats(chats => [...chats, data.val()])
+            setTimeout(() => {
+                updateHeight()
+            }, 100)
+        });
+    }, [])
+
+
+
+    const sendChat = () => {
+        const chatRef = push(chatListRef);
+        set(chatRef, {
+            name, message: msg
+        });
+        setmsg('');
+    };
+
+    return (
+        <div>
+
+
+            {
+                name ?
+                    null :
+                    <div className='home'>
+                        <div className='header'>
+
+                            <section class="wrapper">
+                            <div class="top">Chatsi</div>
+                            <div class="bottom" aria-hidden="true">Chatsi</div>
+                            </section>
+                            
+                            <span className='extra' >Welcome to the open community chats</span>
+                        </div>
+                        <br/>
+                        
+                        <div className='enter'>
+                            <input 
+                                type="text"
+                                name="name"
+                                id="name"
+                                placeholder='Enter the id'
+                                onBlur={e => setname(e.target.value)}>
+                            </input>
+                            <button>Go</button>
+                        </div>
+                        <img
+                            class='center'
+                            style={{ paddingTop:"5px",height: 150, width: 150 }}
+                            src={ak} alt="Ak" />
+
+                    </div>
+            }
+
+            {name ?
+                <div>
+                    <div>
+                        <h1 className='welcome'>Hey {name}! Let's Chat. </h1>
+                    </div>
+                    
+                    <div id='chat' className='chat-container'>
+                        {
+                            chats.map(
+                                // change c
+                                (c, i) => <div key={i} className={`container ${c.name === name ? 'me' : ''}`}>
+                                    <p className='chat-box'>
+                                        <strong>{c.name} : </strong>
+                                        <span>{c.message} </span>
+                                    </p>
+                                </div>)
+                        }
+
+                    </div>
+                    <div className='bot'>
+                        {/* change the function parameter e */}
+                        <input
+                            style ={{width:"80%"}}
+                            type="text"
+                            placeholder='Enter message'
+                            onInput={e => setmsg(e.target.value)}
+                            value={msg} />
+                        <button 
+                        style ={{width:"20%"}}
+                        onClick={e => sendChat()}>Send&#8250;</button>
+                    </div>
+                </div>
+                : null}
+            
+        </div>
+    )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import { createRoot } from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyApijm9WyUZ1AHJ8fpH37a_G242MNfMZxw",
+  authDomain: "chatsi-49fe9.firebaseapp.com",
+  databaseURL: "https://chatsi-49fe9-default-rtdb.firebaseio.com",
+  projectId: "chatsi-49fe9",
+  storageBucket: "chatsi-49fe9.appspot.com",
+  messagingSenderId: "94273278250",
+  appId: "1:94273278250:web:a4ac797465a88a4d4f8e53"
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const root = createRoot(document.getElementById('root'));
+root.render(<App/>)
+
